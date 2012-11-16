dd50ce82d12888f48e55a6bc16c16ad3ae8f441a
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-11-16 11:59:00 -0800

    Remove debug prints.

--- Test/SSOTest.cs
+++ Test/SSOTest.cs
@@ -11,9 +11,8 @@ namespace Test
             user["guid"] = 12343;
             user["expires"] = "2012-12-12 12:12:12";
 
-            System.Console.WriteLine(user);
             string token = UserVoice.SSO.GenerateToken(Config["subdomain_name"], Config["sso_key"], user);
-            System.Console.WriteLine(string.Format("https://{0}.uservoice.com?sso={1}", Config["subdomain_name"], token));
+            //System.Console.WriteLine(string.Format("https://{0}.uservoice.com?sso={1}", Config["subdomain_name"], token));
             AssertTrue(token.Length > 10);
         }
     }
