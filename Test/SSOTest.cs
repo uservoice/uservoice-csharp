468e28775664a0001eaf1f5808495b68c42dfad6
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-11-19 18:33:59 -0800

    Allow valid_for in UserVoice.SSO.GenerateToken.

--- Test/SSOTest.cs
+++ Test/SSOTest.cs
@@ -10,9 +10,8 @@ namespace Test
             string token = UserVoice.SSO.GenerateToken(Config["subdomain_name"], Config["sso_key"], new {
                 email = "tester@example.com",
                 trusted = true,
-                guid = 12343,
-                expires = "2012-12-12 12:12:12"
-            });
+                guid = 12343 //, expires = "2012-12-12 12:12:12"
+            }, 5);
             System.Console.WriteLine(string.Format("https://{0}.uservoice.com?sso={1}", Config["subdomain_name"], token));
             AssertTrue(token.Length > 10);
         }
--- UserVoice/SSO.cs
+++ UserVoice/SSO.cs
@@ -3,13 +3,13 @@ using System.Web;
 using System.Text;
 using System.IO;
 using System.Security.Cryptography;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace UserVoice {
     public class SSO {
         public static string GenerateToken(string subdomainName, string ssoKey, Object userAttributes, int valid_for=5*60) {
-            Console.WriteLine(valid_for);
             string initVector = "OpenSSL for Ruby"; // DO NOT CHANGE
 
             byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
@@ -19,8 +19,14 @@ namespace UserVoice {
             }
             byte[] keyBytes = new byte[16];
             Array.Copy(keyBytesLong, keyBytes, 16);
+            var jsonText = JsonConvert.SerializeObject(userAttributes);
+            Dictionary<string, dynamic> dict = JsonConvert.DeserializeObject<Dictionary<string, dynamic> >(jsonText);
+            if (!dict.ContainsKey("expires")) {
+                dict["expires"] = DateTime.UtcNow.AddSeconds(valid_for).ToString("u");
+                Console.WriteLine("Setting expires " + dict["expires"]);
+            }
 
-            byte[] textBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(userAttributes));
+            byte[] textBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(dict));
             for (int i = 0; i < 16; i++) {
                 textBytes[i] ^= initVectorBytes[i];
             }
