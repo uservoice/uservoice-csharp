1ae0b300fa8d1385321421bd7b56d7e7335ddc24
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-11-20 18:10:33 -0800

    Clean tests.

--- Test/ClientTest.cs
+++ Test/ClientTest.cs
@@ -5,35 +5,35 @@ namespace Test
 
     public class ClientTest: Test {
 
-string callbackURL = "http://localhost:3000/"; // your site
-string USERVOICE_SUBDOMAIN = Config["subdomain_name"];
-string SSO_KEY = Config["sso_key"];
-string API_KEY = Config["api_key"];
-string API_SECRET = Config["api_secret"];
+        string callbackURL = "http://localhost:3000/"; // your site
+        string USERVOICE_SUBDOMAIN = Config["subdomain_name"];
+        string SSO_KEY = Config["sso_key"];
+        string API_KEY = Config["api_key"];
+        string API_SECRET = Config["api_secret"];
 
         public void ShouldGetCurrentUserEmail() {
-            UserVoice.Client client = new UserVoice.Client(Config["subdomain_name"], Config["api_key"], Config["api_secret"]);
+            UserVoice.Client client = new UserVoice.Client(USERVOICE_SUBDOMAIN, API_KEY, API_SECRET);
             client = client.LoginAs("user@example.com");
             AssertEqual("user@example.com", (string)client.Get("/api/v1/users/current")["user"]["email"]);
         }
 
         public void ShouldLoginAsOwner() {
-            UserVoice.Client client = new UserVoice.Client(Config["subdomain_name"], Config["api_key"], Config["api_secret"]);
+            UserVoice.Client client = new UserVoice.Client(USERVOICE_SUBDOMAIN, API_KEY, API_SECRET);
             client = client.LoginAsOwner();
             AssertTrue((bool)client.Get("/api/v1/users/current")["user"]["roles"]["owner"]);
         }
 
         public void ShouldGet10Users() {
-            UserVoice.Client client = new UserVoice.Client(Config["subdomain_name"], Config["api_key"], Config["api_secret"]);
+            UserVoice.Client client = new UserVoice.Client(USERVOICE_SUBDOMAIN, API_KEY, API_SECRET);
             AssertEqual(client.Get("/api/v1/users")["users"].Count(), 10);
         }
 
         public void ShouldReuseAccessToken() {
             const string email = "user@example.com";
-            UserVoice.Client client = new UserVoice.Client(Config["subdomain_name"], Config["api_key"], Config["api_secret"]);
+            UserVoice.Client client = new UserVoice.Client(USERVOICE_SUBDOMAIN, API_KEY, API_SECRET);
             UserVoice.Client userToken = client.LoginAs(email);
 
-            UserVoice.Client copiedToken = new UserVoice.Client(Config["subdomain_name"], Config["api_key"], Config["api_secret"])
+            UserVoice.Client copiedToken = new UserVoice.Client(USERVOICE_SUBDOMAIN, API_KEY, API_SECRET)
                 .LoginWithAccessToken(userToken.Token, userToken.Secret);
             AssertEqual((string)copiedToken.Get("/api/v1/users/current")["user"]["email"], email);
         }
