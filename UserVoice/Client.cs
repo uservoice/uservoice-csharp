40978336c835abdd66ccf4de789e39cd6dc7f0d8
Paul Lemke <plemke@acculynx.com>
2016-01-18 08:51:41 -0600

    Upgrade to newer versions of Newtonsoft.Json and RestSharp to prevent issue with "set_authenticator" error.

--- UserVoice/Client.cs
+++ UserVoice/Client.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using RestSharp.Authenticators;
-using RestSharp.Contrib;
+using System.Web;
 
 namespace UserVoice
 {
